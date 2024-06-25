503bf7ddf6f7510ae4101b3bf38abaa08161e893
Raphael Jamil <raphael.jamil@sga.pucminas.br>
2024-06-25 20:42:52 -0300

    Finalizado

--- codigo/assets/js/app.js
+++ codigo/assets/js/app.js
@@ -27,7 +27,7 @@ function leDados() {
                 description:"super popular no mundo, pela sua refrescancia e leveza, com um amargor equilibrado com sua cor clara, é a preferida de muitos em paises quentes e humidos",
                 nacionalidade: "holanda",estilo: "american lager", fermentacao:"lager", ibu: "17", abv:"4.0", imagem:"assets/images/heineken.jpeg",estrelas:0},
             {
-                id: 3,
+                id: 2,
                 name: "Kaiser",
                 votos:0,
                 description: "KaiserÂ traz uma combinaÃ§Ã£o Ãºnica de Ã¡gua cristalina, lÃºpulos importados e maltes especiais selecionados nas melhores maltarias do mundo.",
@@ -35,7 +35,7 @@ function leDados() {
             },
         
             {
-                id: 4,
+                id: 3,
                 votos:0,
                 name: "Budweiser",
                 description: "A cerveja Budweiser americanaÂ Ã© uma cerveja do estilo Standard American Lager de sabor leve, cor clara, aroma discreto, baixo amargor e 5% de teor alcoÃ³lico.",
@@ -43,7 +43,7 @@ function leDados() {
             },
         
             {
-                id: 6,
+                id: 4,
                 votos:0,
                 name: "Stella Artois",
                 description: "Cristalina, recebeu o nome deÂ Stella, estrela em Latim. Sua receita Ãºnica produz umaÂ cervejaÂ com aromas suaves e notas maltadas.",
@@ -51,7 +51,7 @@ function leDados() {
             },
         
             {
-                id: 7,
+                id: 5,
                 votos:0,
                 name: "Eisenbahn",
                 description: "A EisenbahnÂ foi fundada por Juliano Mendes, que passou alguns anos explorando o universo cervejeiro pelo mundo e notou uma ausÃªncia de cervejas artesanais de qualidade no Brasil.",
@@ -59,7 +59,7 @@ function leDados() {
             },
         
             {
-                id:8,
+                id:6,
                 name: "Petra",
                 votos:0,                           
                  description: "Ã‰ umaÂ cervejaÂ de cor dourada, colarinho branco bem formado e persistente. No odor e paladar sua principal caracterÃ­stica Ã© o malte.",
@@ -67,7 +67,7 @@ function leDados() {
             },
         
             {
-                id: 9,
+                id: 7,
                 name: "Original",
                 votos:0,
                 description: "A histÃ³ria daÂ cerveja OriginalÂ comeÃ§ou em 1906, quando um jovem recÃ©m-chegado da Alemanha, Henrique Thielen, fundou a Cervejaria AdriÃ¡tica, em Ponta Grossa.",
@@ -76,7 +76,7 @@ function leDados() {
         
             
             {
-                id: 10,
+                id: 8,
                 name: "Bohemia" ,
                 votos:0,
                 description: "A histÃ³ria se inicia em 1853, na cidade de PetrÃ³polis, quando a primeira cervejaria do Brasil foi criada.Â BohemiaÂ viu a cultura do paÃ­s em movimento, uma cultura de sabedoria popular, de saberes dos mais diversos.",
@@ -84,7 +84,7 @@ function leDados() {
             },
         
             {
-                id: 11,
+                id: 9,
                 name: "Itaipava ",
                 votos:0,
                 description: "Produzida com ingredientes 100% selecionados, Ã¡gua 100% cristalina e respeitando o tempo certo, a ItaipavaÂ Ã© uma cerveja clara, leve, saborosa e refrescante.",
@@ -92,7 +92,7 @@ function leDados() {
             },
         
             {
-                id: 12,
+                id: 10,
                 name: "Devassa" ,
                 votos:0,
                 description: "DevassaÂ Ã© uma cerveja puro malte tropical, ou seja, feita apenas com Ã¡gua, malte, lÃºpulo e um toque tropical. Mas o que significa â€œtropicalâ€? Ã‰ o segredo para produzir um puro malte adaptado ao clima e paladar brasileiro.",
@@ -100,7 +100,7 @@ function leDados() {
             },
         
             {
-                id: 13,
+                id: 11,
                 name: "Skol",
                 votos:0,                           
                 description: "SkolÂ Ã© uma marca deÂ cervejaÂ de propriedade da empresa dinamarquesa Carlsberg, com licenÃ§a para ser fabricada no Brasil.",
@@ -108,7 +108,7 @@ function leDados() {
             },
         
             {
-                id: 14,
+                id: 12,
                 name: "Crystal" ,
                 votos:0,
                 description: "CrystalÂ Ã© uma cerveja leve, cristalina, preparada com ingredientes cuidadosamente selecionados e matÃ©rias-primas especiais. AlÃ©m de todos esses atributos, ela tambÃ©m Ã© pioneira: foi a primeira cerveja do Brasil a receber o selo de proteÃ§Ã£o no bocal das suas latas.",
@@ -116,7 +116,7 @@ function leDados() {
             },
         
             {
-                id: 15,
+                id: 13,
                 name: "Nova Schin",
                 votos:0,
                 description: "Considerada uma das marcas mais tradicionais e famosas do Brasil, a Schincariol faz parte de uma histÃ³ria nÃ£o apenas na vida dos Brasileiros, mas de um segmento que cresceu e se popularizou graÃ§as a marcas como a Schin.",
@@ -125,7 +125,7 @@ function leDados() {
         
         
             {
-                id: 16,
+                id: 14,
                 name: "Bavaria" ,
                 votos:0,
                 description: "A Cerveja Pilsen BavariaÂ Ã© de origem da regiÃ£o da BavÃ¡ria, na Alemanha, feita com uma combinaÃ§Ã£o de maltes selecionados e 100% de lÃºpulos aromÃ¡ticos de Hallertau.",
@@ -134,7 +134,7 @@ function leDados() {
         
         
             {
-                id: 17,
+                id: 15,
                 name: "ImpÃ©rio ",
                 votos:0,
                 description: "Com teor alcoÃ³lico de 4,8%, Ã© uma cerveja leve e refrescante, ideal para acompanhar momentos de descontraÃ§Ã£o e diversÃ£o. Apresentada em latas de 350ml, a Cerveja Puro Malte Pilsen ImpÃ©rio Ã© uma escolha perfeita para quem busca uma experiÃªncia Ãºnica de degustaÃ§Ã£o.",
@@ -143,7 +143,7 @@ function leDados() {
         
         
             {
-                id: 18,
+                id: 16,
                 name: "Spaten",  
                 votos:0,
                  description: "SPATEN: Autoridade em cerveja.Â Uma das primeiras puro malte do mundo e uma das cervejarias oficiais da Oktoberfest de Munique, na Alemanha. Feita com um rigoroso processo, seguindo a receita de Munique, onde foi criada em 1397. Uma cerveja puro malte com um sabor marcante e amargor presente. ",
@@ -151,7 +151,7 @@ function leDados() {
             },
         
             {
-                id: 19,
+                id: 17,
                 name: "Corona", 
                 votos:0,                           
                  description: "Esta cerveja do tipo Pilsen foi produzida pela primeira vez em 1925 pela CervecerÃ­a Modelo, localizada na Cidade do MÃ©xico.Â Possui sabor leve de baixa graduaÃ§Ã£o alcoÃ³lica.",
